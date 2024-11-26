7eac0df01beeba563d35f0b43fe13e06b6bb0932
Denilson2619 <p.denilson@escolar.ifrn.edu.br>
2024-11-25 22:50:27 -0300

    WIP - telas novas e organizadas

--- scripts.js
+++ scripts.js
@@ -13,3 +13,32 @@ barbeiroBtn.addEventListener('click', () => {
     form.style.display = 'block';
     camposBarbeiro.style.display = 'block';
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("form-agendamento");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Impede o envio tradicional do formulário
+
+        // Redireciona para a página 'revisar.html'
+        window.location.href = "revisar.html"; // Página de revisão
+    });
+});
+
+// Função para capturar parâmetros da URL
+function getUrlParams() {
+    const params = new URLSearchParams(window.location.search);
+    return {
+        endereco: params.get('endereço'),
+        data: params.get('data'),
+        horario: params.get('horario'),
+        barbeiro: params.get('barbeiro')
+    };
+}
+
+// Preenche os campos com os valores passados na URL
+const params = getUrlParams();
+document.getElementById('endereco').innerText = params.endereco;
+document.getElementById('data').innerText = params.data;
+document.getElementById('horario').innerText = params.horario;
+document.getElementById('barbeiro').innerText = params.barbeiro;
